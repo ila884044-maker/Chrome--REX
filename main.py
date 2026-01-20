93eab8eb795b0deb89d08bbc525c5602ddbe6fc5
ila884044-maker <ila884044@gmail.com>
2026-01-21 02:48:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+import os
+import subprocess
 
+def open_src_and_run_dino():
+    # Определяем путь к папке src относительно текущего скрипта
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    src_folder_path = os.path.join(current_dir, 'src')
+    
+    if not os.path.exists(src_folder_path):
+        print(f"Папка {src_folder_path} не найдена.")
+        return
+    
+    # Переходим в папку src
+    os.chdir(src_folder_path)
+    
+    # Запускаем файл dino.py
+    try:
+        subprocess.run(['python', 'dino.py'], check=True)
+    except Exception as e:
+        print(f"Ошибка при выполнении dino.py: {e}")
+
+if __name__ == "__main__":
+    open_src_and_run_dino()
